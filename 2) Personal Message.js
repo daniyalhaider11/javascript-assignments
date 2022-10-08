9249adddedf1c248ebdfa389eb9abba0f3c1deec
Daniyal Haider <43630211+daniyalhaider11@users.noreply.github.com>
2022-10-08 19:17:58 +0500

    Delete 2) Personal Message.js

--- 2) Personal Message.js
+++
@@ -1,7 +0,0 @@
-// QUESTION NO: Store a person’s name in a variable, and print a message to that person. 
-// Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
-
-// AWNSER: 
-
-let name = "Daniyal Haider";
-console.log('"Hello," ${name}, " would you like to learn some Javascript today?"');
