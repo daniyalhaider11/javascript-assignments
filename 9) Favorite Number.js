ab139ac9d59d439b26cf9353a7bff9468e39f612
Daniyal Haider <43630211+daniyalhaider11@users.noreply.github.com>
2022-10-08 19:19:23 +0500

    Delete 9) Favorite Number.js

--- 9) Favorite Number.js
+++
@@ -1,9 +0,0 @@
-// QUESTION: Store your favorite number in a variable. 
-// Then, using that variable, create a message that reveals your favorite number. 
-// Print that message.
-
-// AWNSER:
-
-const favNumber = 8;
-
-console.log(`My favorite number is: ${favNumber}`);
